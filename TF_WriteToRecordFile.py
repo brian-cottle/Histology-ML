a035f5b1b92d78eaff8d95a5354c058746dcb52e
brian-cottle <brian.k.cottle@icloud.com>
2022-06-02 17:00:47 -0600

    thoroughly commented first functional file

--- TF_WriteToRecordFile.py
+++ TF_WriteToRecordFile.py
@@ -20,6 +20,9 @@ import matplotlib.pyplot as plt
 #############################################################
 #############################################################
 
+# These following functions were created from the TDS blog and 
+# the tensorflow suggestions/tutorial
+
 def int64_feature(value):
   """Returns an int64_list from a bool / enum / int / uint."""
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -31,7 +34,7 @@ def float_feature_list(value):
 
 def bytes_feature(value):
     """Returns a bytes_list from a string / byte."""
-    if isinstance(value, type(tf.constant(0))): # if value ist tensor
+    if isinstance(value, type(tf.constant(0))): # if value is tensor
         value = value.numpy() # get value of tensor
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
@@ -65,6 +68,10 @@ def get_bounding_boxes(binary_image):
     labeled_image = measure.label(binary_image) # labeling image
     regions = measure.regionprops(labeled_image) # getting region props
     
+    # lists for storing the sequence of x, y, width, and height data for 
+    # the image. They are separate so that each can be its own list that 
+    # is stored, seemed to make sense with the limitations of the 
+    # float_feature_list thing. 
     box_x = []
     box_y = []
     box_width = []
@@ -84,7 +91,7 @@ def get_bounding_boxes(binary_image):
         width = bounding_box[2]-bounding_box[0]
         height = bounding_box[3]-bounding_box[1]
 
-        # appending to list for storage
+        # appending to respective lists for storage
         box_x.append(x)
         box_y.append(y)
         box_width.append(width)
@@ -111,11 +118,15 @@ def load_image_names(folder):
 #############################################################
 
 def parse_image_bbox(file_name,bbox_class_id):
+    # This function receives a name and the class id of which you want to 
+    # provide bounding boxes for in the dataset
     image = cv.imread(file_name,cv.IMREAD_UNCHANGED)
-    color_image = image[:,:,0:3]
+    # getting the segmentation for the bbox production
     seg = image[:,:,3]
+    # creating the binary image for bboxes
     bbox_seg = seg == bbox_class_id
     bbox = get_bounding_boxes(bbox_seg)
+
     parsed_image = parse_image(image,bbox)
 
     return(parsed_image)
@@ -123,7 +134,12 @@ def parse_image_bbox(file_name,bbox_class_id):
 #############################################################
 
 def get_shard_sizes(file_names,max_files_per_shard):
+    # This function receives a list of file names and the maximum number of 
+    # files you want to save in a shard.
     num_images = len(file_names)
+    # determining the number of splits for the image. The +1 collects any 
+    # stragglers that don't completely "fill up" a shard. It is removed if the 
+    # number comes out with no remainder. 
     num_splits = num_images//max_files_per_shard + 1
     if num_images%max_files_per_shard == 0:
         num_splits -= 1
@@ -138,27 +154,43 @@ def write_all_images_to_shards(file_names,
                                num_splits,
                                max_files_per_shard,
                                bbox_id=5):
-
+    # this function receives a list of file names, the number of splits
+    # produced by get_shard_sizes, the how many files will be put in each
+    # shard, and the class id of which segmentation you want to produce
+    # bounding boxes for. In the future you could easily add the functionality
+    # to produce bboxes for both vasculature and neural tissues.
     out_directory = './../dataset_shards/'
+
+    # create the directory for saving if it doesn't already exist
     if not os.path.isdir(out_directory):
         os.mkdir(out_directory)
-    
+
+    # keeps track of how many files have been written total    
     files_written = 0
 
+    # displaying progress based on number of shards successfully saved
     for idx in tqdm.tqdm(range(num_splits)):
+    
         current_shard_name = out_directory + \
             f'shard_{idx+1}_of_{num_splits}.tfrecords'
         
+        # create the writer for the current shard
         writer = tf.io.TFRecordWriter(current_shard_name)
         
+        # keep track of how many files we've put in this shard
         num_files_this_shard = 0
 
+        # exit if we hit the max number of files for this shard
         while num_files_this_shard < max_files_per_shard:
+            # keeping track of names of files across shards
             image_idx = idx*max_files_per_shard + num_files_this_shard
+            # if we hit the end of all the files, stop this shard
             if image_idx == len(file_names):
                 break
+            # get a parsed image
             parsed_image = parse_image_bbox(file_names[image_idx],bbox_id)
 
+            # add the current image to the tfrecord file
             writer.write(parsed_image.SerializeToString())
 
             num_files_this_shard += 1
@@ -172,7 +204,10 @@ def write_all_images_to_shards(file_names,
 #############################################################
 
 def parse_tf_elements(element):
+    # this function is the mapper function for retrieving examples from
+    # the tfrecord
 
+    # create placeholders for all the features in each example
     data = {
         'height' : tf.io.FixedLenFeature([],tf.int64),
         'width' : tf.io.FixedLenFeature([],tf.int64),
@@ -184,8 +219,10 @@ def parse_tf_elements(element):
         'bbox_width' : tf.io.VarLenFeature(tf.float32)
     }
 
+    # pull out the current example
     content = tf.io.parse_single_example(element, data)
 
+    # pull out each feature from the example 
     height = content['height']
     width = content['width']
     raw_seg = content['raw_seg']
@@ -195,11 +232,15 @@ def parse_tf_elements(element):
     bbox_height = content['bbox_height']
     bbox_width = content['bbox_width']
 
+    # convert the images to uint8, and reshape them accordingly
     image = tf.io.parse_tensor(raw_image, out_type=tf.uint8)
     image = tf.reshape(image,shape=[height,width,3])
     segmentation = tf.io.parse_tensor(raw_seg, out_type=tf.uint8)
     segmentation = tf.reshape(segmentation,shape=[height,width,1])
 
+    # there currently is a bug with returning the bbox, but isn't necessary
+    # to fix for creating the initial uNet for segmentation exploration
+    
     # bbox = [bbox_x,bbox_y,bbox_height,bbox_width]
 
     return(image,segmentation) # 
@@ -208,7 +249,7 @@ def parse_tf_elements(element):
 #############################################################
 
 # %%
-
+# writing the files to a new directory!
 dataset_directory = '/media/briancottle/Samsung_T5/sub_sampled_20220531'
 os.chdir(dataset_directory)
 file_names = load_image_names(dataset_directory)
@@ -216,17 +257,14 @@ num_splits,max_files_per_shard = get_shard_sizes(file_names,500)
 
 write_all_images_to_shards(file_names,num_splits,max_files_per_shard,bbox_id=5)
 
-# %%
+# %% loading an example shard, and creating the mapped dataset
 os.chdir('/media/briancottle/Samsung_T5/dataset_shards')
-dataset = tf.data.TFRecordDataset('shard_5_of_21.tfrecords')
+dataset = tf.data.TFRecordDataset('shard_15_of_21.tfrecords')
 dataset = dataset.map(parse_tf_elements)
 # %%
+# double checking some of the examples to make sure it all worked well!
 for sample in dataset.take(2):
     plt.imshow(sample[0])
     plt.show()
     plt.imshow(sample[1])
     plt.show()
-# %%
-
-B = dataset.take(30)
-# %%
